3df37f9fa5a19feaa65a297720653995c4cf3ae6
Evalds <evalds@efumo.lv>
2017-04-10 18:19:18 +0300

    3.8 catch wrogn URL

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -50,13 +50,21 @@ class GenusController extends Controller
     }
 
     /**
-     * @Route("/genus/{genusName}")
+     * @Route("/genus/{genusName}", name="genus_show")
      */
     public function showAction($genusName)
     {
-        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+        $em = $this->getDoctrine()->getManager();
+        $genus = $em->getRepository('AppBundle:Genus')
+            ->findOneBy(['name' => $genusName]);
 
-        $cache = $this->get('doctrine_cache.providers.my_evald_cache');
+        if (!$genus) {
+            throw $this->createNotFoundException('No genus found');
+        }
+
+        /*$funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';*/
+
+        /*$cache = $this->get('doctrine_cache.providers.my_evald_cache');
         $key = md5($funFact);
         if($cache->contains($key)){
             $funFact = $cache->fetch($key);
@@ -65,13 +73,12 @@ class GenusController extends Controller
             $funFact = $this->get('markdown.parser')
                 ->transform($funFact);
             $cache->save($key, $funFact);
-        }
+        }*/
 
 
 
         return $this->render('genus/show.html.twig', [
-            'name' => $genusName,
-            'funFact' => $funFact,
+            'genus' => $genus
         ]);
 
     }
