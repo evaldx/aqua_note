3c7540767f96e6cf117f6e97d102cf5fedea43bb
Evalds <evalds@efumo.lv>
2017-04-13 13:43:22 +0300

    3.10 Fixtures Alice fake data

--- src/AppBundle/DataFixtures/ORM/LoadFixtures.php
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -11,18 +11,13 @@ namespace AppBundle\DataFixtures\ORM;
 use AppBundle\Entity\Genus;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Fixtures;
 
 
 class LoadFixtures implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $genus = new Genus();
-        $genus->setName('Octopus'.rand(1, 100));
-        $genus->setSubFamily('Octopodienae');
-        $genus->setSpeciesCount(rand(100, 99999));
-
-        $manager->persist($genus);
-        $manager->flush();
+        Fixtures::load(__DIR__.'/fixtures.yml', $manager);
     }
 }
