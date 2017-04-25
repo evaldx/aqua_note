4ec2c0caa34f04feb8c50d0a2f2da52bb3a4724d
Evalds <evalds@efumo.lv>
2017-04-25 14:04:25 +0300

    7.3 rendering login form

---
+++ src/AppBundle/Form/LoginForm.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: evalds
+ * Date: 17.25.4
+ * Time: 13:49
+ */
+
+namespace AppBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class LoginForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('_username')
+            ->add('_password', PasswordType::class);
+    }
+}
